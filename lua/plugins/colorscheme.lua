f33de296d29e7c22302b2d25cd0cb860db248198
Alex Coté <alex.cote@tuta.io>
2023-02-15 22:58:02 -0600

    UI and colorscheme changes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
